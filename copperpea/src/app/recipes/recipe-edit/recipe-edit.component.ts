067c2f46696dc26b6d6c3bbf20d214dcdf3d7f65
huajwang <cse63152@gmail.com>
2020-04-17 18:19:41 -0400

    populate a list of items to FormArray;

--- copperpea/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ copperpea/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from "@angular/router";
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormArray } from "@angular/forms";
 
 import { RecipeService } from "../recipe.service";
 
@@ -31,22 +31,36 @@ export class RecipeEditComponent implements OnInit {
     let rcName = '';
     let rcDesc = '';
     let rcImagePath = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const rc = this.rcService.getRecipe(this.id);
       rcName = rc.name;
       rcDesc = rc.description;
       rcImagePath = rc.imagePath;
+
+      if (rc['ingredients']) {
+        for (let ingredient of rc.ingredients) {
+          // populate FormArray with FormGroups
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount)
+            })
+          );
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       name: new FormControl(rcName),
       description: new FormControl(rcDesc),
-      imagePath: new FormControl(rcImagePath)
+      imagePath: new FormControl(rcImagePath),
+      ingredients: recipeIngredients
     });
   }
 
   onSubmit() {
-    
+
   }
 
 }
