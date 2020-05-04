05902b40e08da9679d6c083cd5964a9312a960c0
huajwang <cse63152@gmail.com>
2020-05-03 21:40:11 -0400

    implment chat function;

--- copperpea/src/app/core/core.module.ts
+++ copperpea/src/app/core/core.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "../app-routing.module";
 
@@ -15,7 +16,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   exports: [
     AppRoutingModule,
--- copperpea/src/app/core/home/home.component.ts
+++ copperpea/src/app/core/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('chatPopup') chatPopup: ElementRef;
+  @ViewChild('box') box: ElementRef;
 
-  constructor() { }
+  private conversations = [
+    {who: 'host', sentence: 'Hello. How are you today?'},
+    {who: 'guest', sentence: 'Can you help me?'}
+  ];
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
   }
 
+  openChat() {
+    //this.chatPopup.nativeElement.style.display="block"
+    this.renderer.setStyle(this.chatPopup.nativeElement, 'display', 'block');
+    // f.form.patchValue({msg: '你好， 欢迎!'});
+  }
+
+  onEnter(value: String) {
+    this.conversations.push({who: 'guest', sentence: value});
+    // this.box.nativeElement.value = "";
+    this.renderer.setProperty(this.box.nativeElement, 'value', '');
+  }
+
+  // closeForm() {
+  //   this.renderer.setStyle(this.chatPopup.nativeElement, 'display', 'none');
+  // }
+  //
+  // onSent(f: NgForm) {
+  //   console.log(f.value.msg);
+  // }
 }
