bc70af67247f2309567a56e8ce84e2476c0d391e
huajwang <cse63152@gmail.com>
2020-05-28 09:42:39 -0400

    add security;

--- copperpea/src/app/me/me-routing.module.ts
+++ copperpea/src/app/me/me-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from "@angular/router";
 import { MeComponent } from "./me.component";
 import { MeCurrentComponent } from "./me-current/me-current.component";
 import { MeStartComponent } from "./me-start/me-start.component";
+import { AuthGuardService } from "../auth/auth-guard.service";
 
 const meRoutes: Routes = [
   {path: '', component: MeComponent, children: [
     {path: '', component: MeStartComponent },
     {path: 'current', component: MeCurrentComponent}
-  ]},
+  ], canActivate: [AuthGuardService]},
 ];
 
 @NgModule({
