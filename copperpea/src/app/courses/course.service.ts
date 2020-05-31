8ab88540eaf96f39fcef15e70c1c2c28ee186f8f
huajwang <cse63152@gmail.com>
2020-05-30 21:01:30 -0400

    add http client to retrieve backend data;

--- copperpea/src/app/courses/course.service.ts
+++ copperpea/src/app/courses/course.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 import { Course } from "./course.model";
 
 @Injectable({
@@ -6,13 +8,15 @@ import { Course } from "./course.model";
 })
 export class CourseService {
 
+  courseUrl = 'http://localhost:8080/edu/course_content';
+  constructor(private http: HttpClient) {}
+
+
   pyCourses: Course[] = [
     new Course("Python入门", "学会分析抽象问题与解决抽象问题的能力。", "/assets/img/python.jpg"),
     new Course("Python进阶", "进一步提升计算思维，熟练掌握分析抽象问题和解决抽象问题能力。", "/assets/img/python.jpg"),
     new Course("Python竞赛", "强化实战中的计算思维，熟练掌握在实战中去计算思维去发现问题、分析问题和解决问题的能力。", "/assets/img/python.jpg")
   ];
-
-  constructor() { }
 
   getCourses(courseName: String) {
     if (courseName == 'python') {
